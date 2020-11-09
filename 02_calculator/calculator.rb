6a97dd4f2411983b9cb078c19f34236f91aa5703
Iuliia Shevtsova <uliyaal2018@gmail.com>
2020-11-09 13:03:15 -0500

    00 - 05 tasks completed

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,76 @@
 #write your code here
+def add(x, y)
+    x+y
+end
+
+def subtract(x, y)
+    x-y
+end
+
+# sum ****************
+
+# sum -- 1 way
+# def sum(array)
+#     array.sum
+# end
+
+# sum -- 2 way
+def sum(array)
+    sum = 0
+    array.each{|x| sum += x}
+    sum
+end
+
+
+# * `multiply`********
+
+# for two numbers
+# def multiply(x, y)
+#     x * y
+# end
+  
+# for several numbers
+def multiply(array)
+    m = 1
+    array.each{|x| m *= x}
+    m
+end
+
+# * `power`
+def power(a, x)
+    p=1
+    x.times do
+      p*=a
+    end
+    p
+end
+
+# * `[factorial] *************
+# 1 way -- block
+
+# def factorial(x, &block)
+#     x < 1? 1 : block.call(x)
+# end
+
+# fact = factorial(5) do |x|
+#     f=1
+#     for i in 1..x do 
+#         f*=i
+#     end
+#     f
+# end
+
+# puts fact
+
+# 2 way
+def factorial(x)
+    if x < 1 
+      f =1 
+    else 
+      f=1
+      for i in 1..x do 
+        f*=i
+      end
+    end
+    f
+end
--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,59 @@
 #write your code here
+def translate(words)
+    word = words.split.map{ |item| new_word(item)}.join(" ")
+    
+  end
+  
+  def vowel(letter) #check if first letter is vowel
+    ifvowel = false
+    vowels = ["a", "e", "i", "o", "u"]
+    vowels.each do |vowel| 
+      if letter == vowel
+        ifvowel = true
+      end
+    end
+    ifvowel
+end
+
+def consonant(letters) #check if first several letters are consonants
+  letters = letters.split('') if letters.length > 0 # array of first letters
+  i = 0
+  count = letters.length
+  ifconsonant = false
+  vowels = ["a", "e", "i", "o", "u"]
+  
+  while i < letters.length do
+      # puts letters[i]
+      vowels.each do |vowel|
+          if letters[i] == vowel 
+              count -= 1
+          end
+      end
+      i += 1
+  end
+  
+  if count == letters.length
+      ifconsonant = true
+  end
+  return ifconsonant
+end
+
+def new_word(word)
+  new_word = ""
+  if vowel(word.slice(0)) 
+    new_word = word + "ay"
+  elsif consonant(word.slice(0..2)) || (word.slice(1..2) == "qu")
+    first_letters = word.slice(0..2)
+    letters = word.slice(3..(word.length))
+    new_word = letters + first_letters + "ay"
+  elsif (word.slice(0..1) == "qu") || (consonant(word.slice(0..1)))
+    first_letters = word.slice(0..1)
+    letters = word.slice(2..(word.length))
+    new_word = letters + first_letters + "ay"
+  elsif consonant(word.slice(0))
+    first_letter = word.slice(0)
+    letters = word.slice(1..(word.length))
+    new_word = letters + first_letter + "ay"
+  end
+  new_word
+end
