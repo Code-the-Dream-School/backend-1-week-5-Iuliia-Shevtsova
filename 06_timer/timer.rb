dd6b536808622dc88f1fc73cde68ddca2db01b28
Iuliia Shevtsova <uliyaal2018@gmail.com>
2020-11-09 18:33:27 -0500

    06 completed

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,19 @@
 class Timer
-  #write your code here
+  
+  #should initialize to 0 seconds
+  def seconds
+    @seconds ||= 0
+  end
+  
+  #should initialize seconds by user
+  def seconds=(sec)
+    @seconds = sec
+  end
+
+  # should display 0 seconds as 00:00:00
+  def time_string
+    seconds = @seconds
+    seconds = Time.at(seconds).utc.strftime("%T")
+  end
+
 end
